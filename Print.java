c5a28ab74ac3dd3ba1d78efbe1a9ad779f8b1af9
Amit Dhama <amitdhama2007@gmail.com>
2026-08-23 14:22:54 +0530

    Added Print.java for basic output

--- Print.java
+++ Print.java
@@ -2,6 +2,7 @@ public class Print
 {
   public static void main(String[] args)
   {
+    //for printing output in java
     System.out.println("Hello");
     System.out.println("Algoforge");
     System.out.print("Hii");
